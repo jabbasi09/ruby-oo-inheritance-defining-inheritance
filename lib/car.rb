ae9c4d51449ed727f06e9990eb402b7fd52b722a
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-18 19:32:09 -0600

    commit tree 05736abcff7a9cd9cfe79aef461cb3f96774e4a4

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
+# Child Class
+# We are requiring lib/vehicle.rb. That is because our Car class will need access to the Vehicle class and will therefore need access to the file that contains that class.
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
